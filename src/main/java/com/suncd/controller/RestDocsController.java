4ea4f8389d49859b435a76c1761aa2ae27759ea0
qust <13214123320@163.com>
2017-06-27 18:36:52 +0800

    openvpn配置修改

--- src/main/java/com/suncd/controller/RestDocsController.java
+++ src/main/java/com/suncd/controller/RestDocsController.java
@@ -3,6 +3,7 @@ package com.suncd.controller;
 import com.suncd.entity.ContractDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,7 +24,16 @@ public class RestDocsController {
 
     @RequestMapping(value = "/user/5",method = RequestMethod.GET)
     public ContractDto get(){
-        return new ContractDto("c","plgx");
+        return new ContractDto("c","cplgx");
+    }
+
+    @RequestMapping(value = "/user/{id}",method = RequestMethod.GET)
+    public ContractDto getUser(
+            @RequestParam(value = "title") String title,
+            @RequestParam(value = "title1") String title1
+//            @RequestParam(value = "title2") String title2
+    ){
+        return new ContractDto("c","cplgx"+title1+title);
     }
 
 }
